3f7be3a69545fecdd213ba070e8aa8641548f326
Kyle Padayachee <kyle44987@gmail.com>
2025-07-10 12:16:24 +0200

    implemented class models UserInput and AI Response for data services

---
+++ backend/services/data_service.py
@@ -0,0 +1,10 @@
+from models import PSYCHOLOGY_BOOKS, MEDITATION_BOOKS
+
+class DataService:
+    def get_psychology_books(self):
+        """Get psychology books data"""
+        return PSYCHOLOGY_BOOKS
+
+    def get_meditation_books(self):
+        """Get meditation books data"""
+        return MEDITATION_BOOKS
